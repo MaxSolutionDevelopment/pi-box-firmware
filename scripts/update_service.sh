e394fca3865587e9cba3dc4fb4740d2728fcb165
donhat-dev <donhat.hn@gmail.com>
2025-03-05 03:42:49 +0700

    Update update_service.sh

--- scripts/update_service.sh
+++ scripts/update_service.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 SYSTEMD_PATH="/home/admin/pi-box-firmware/systemd"
-# # Đảm bảo ngrok service đã được enable và start (thiết lập ngoài cron nếu cần)
-# if [ ! -f /etc/systemd/system/ngrok.service ]; then
-#     echo "Creating Ngrok service..."
-#     sudo cp $SYSTEMD_PATH/ngrok.service /etc/systemd/system/
-#     sudo systemctl daemon-reload
-#     sudo systemctl enable ngrok.service
-#     sudo systemctl start ngrok.service
-# else
-#     sudo cp $SYSTEMD_PATH/ngrok.service /etc/systemd/system/
-#     echo "Ngrok service already exists. Updating..."
-#     sudo systemctl daemon-reload
-#     sudo systemctl restart ngrok.service
-# fi
+# Đảm bảo ngrok service đã được enable và start (thiết lập ngoài cron nếu cần)
+if [ ! -f /etc/systemd/system/ngrok.service ]; then
+    ngrok service install --config=/home/admin/ngrok.yml
+    sudo systemctl enable --now ngrok
+    
+else
+    echo "Ngrok service already exists."
+    
+    # sudo cp $SYSTEMD_PATH/ngrok.service /etc/systemd/system/
+    # echo "Ngrok service already exists. Updating..."
+    # sudo systemctl daemon-reload
+    # sudo systemctl restart ngrok.service
+fi
 
 # Kiểm tra và tạo service cho update-monitor
 if [ ! -f /etc/systemd/system/update-monitor.service ]; then
