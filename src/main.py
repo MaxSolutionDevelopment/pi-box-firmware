b2aeafb2b329e90c7ec4d4e3434db7eb77ddbf0a
donhat-dev <donhat.hn@gmail.com>
2025-03-05 03:39:14 +0700

    []

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import json
 import os
 from pathlib import Path
 import sys
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -14,6 +15,38 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+class NgrokConfig(BaseModel):
+    authtoken: str
+    configpath: str = None
+
+# Đường dẫn đến file ngrok.yml
+NGROK_CONFIG_PATH = "/home/admin/ngrok.yml"
+
+@app.post("/set-ngrok-token")
+def set_ngrok_token(config: NgrokConfig):
+    try:
+        # Tạo cấu hình cho ngrok.yml
+        ngrok_yml_content = f"""version: "3"
+agent:
+    authtoken: {config.authtoken}
+"""
+        config_path = config.configpath if config.configpath else NGROK_CONFIG_PATH
+
+        # # Kiểm tra xem file ngrok.yml đã tồn tại chưa
+        if not Path(NGROK_CONFIG_PATH).is_file():
+            # Nếu chưa tồn tại thì tạo file ngrok.yml
+            Path(NGROK_CONFIG_PATH).touch
+        
+        # Ghi nội dung vào file ngrok.yml
+        with open(NGROK_CONFIG_PATH, "w") as file:
+            file.write(ngrok_yml_content)
+        
+        return {"status": "success", "message": "Ngrok token saved successfully."}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error saving token: {str(e)}")
+
 
 # Healthcheck endpoint
 @app.get("/health")
