fa7c4eb395a534f57cc0f8c3a1b6917ec16db33c
donhat-dev <donhat.hn@gmail.com>
2025-03-05 03:43:51 +0700

    Update update_service.sh

--- scripts/update_service.sh
+++ scripts/update_service.sh
@@ -2,12 +2,12 @@
 SYSTEMD_PATH="/home/admin/pi-box-firmware/systemd"
 # Đảm bảo ngrok service đã được enable và start (thiết lập ngoài cron nếu cần)
 if [ ! -f /etc/systemd/system/ngrok.service ]; then
-    ngrok service install --config=/home/admin/ngrok.yml
+    /snap/bin/ngrok service install --config=/home/admin/ngrok.yml
     sudo systemctl enable --now ngrok
     
 else
     echo "Ngrok service already exists."
-    
+
     # sudo cp $SYSTEMD_PATH/ngrok.service /etc/systemd/system/
     # echo "Ngrok service already exists. Updating..."
     # sudo systemctl daemon-reload
